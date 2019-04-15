0fe9581add9c1e98d7c7d6219bbf76374706e37c
mile <1181254221@qq.com>
2019-04-15 19:35:57 +0800

    把本地工程添加到git

--- Main.java
+++ Main.java
@@ -3,4 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
+
 }
